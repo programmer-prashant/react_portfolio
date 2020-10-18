b664ee8028de4bea37be5cdfe7df4d9c71884a89
Prashant Raut <prashantraut33@gmail.com>
2020-10-18 11:04:29 +0530

    MyCaraosl and Title component completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 
 //Components
 import MyNavbar from './components/Navbar/mynavbar.components';
+import MyCarousel from './components/Carousel/myCarousel.component'
+import TitleMessage from './components/title-message/title-message.component'
+import ScrollDown from './components/scroll-down/scroll-down.component'
+
 const App = () => {
   return (
     <div className="App">
       <MyNavbar />
+      <MyCarousel/>
+      <TitleMessage/>
+      <ScrollDown/>
     </div>
   );
 };
