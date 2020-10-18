b9ebe2be6c2ab9ee8afff186fcb794cfaad48266
Prashant Raut <prashantraut33@gmail.com>
2020-10-18 20:57:55 +0530

    All app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import './App.css';
-
-//Components
 import MyNavbar from './components/Navbar/mynavbar.components';
-import MyCarousel from './components/Carousel/myCarousel.component';
-import TitleMessage from './components/title-message/title-message.component';
-import ScrollDown from './components/scroll-down/scroll-down.component';
+import MyCarousal from './components/Carousel/myCarousel.component';
+import MyTitleMessage from './components/title-message/title-message.component';
 import About from './pages/about/about.component';
-import { Parallax } from 'react-parallax';
-import Container from 'react-bootstrap/Container';
-import Fade from 'react-reveal/Fade';
 import Skills from './pages/skills/skills.component';
+import Container from 'react-bootstrap/Container';
+import TimeLine from './components/projects-timeline/projects-timeline.component';
 import Experience from './pages/experience/experience.component';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+import ContactForm from './pages/contact-form/contact-form.component';
+import { Parallax } from 'react-parallax';
+import FooterPanel from './components/footer/footer.component';
+
+import './App.css';
 
 const App = () => {
 	return (
 		<div className='App' style={{ position: 'relative' }}>
 			<MyNavbar />
-			<MyCarousel />
-			<TitleMessage />
-			<ScrollDown />
+			<MyCarousal />
+			<MyTitleMessage />
+
 			<div>
 				<Parallax
 					blur={{ min: -30, max: 30 }}
@@ -37,20 +39,37 @@ const App = () => {
 				</Parallax>
 			</div>
 
-			<div>
-				<Container className='container-box rounded'>
-					<Fade duration={500}>
-						<Skills />
-					</Fade>
-				</Container>
-			</div>
-			<div>
-				<Container className='container-box rounded'>
-					<Fade duration={500}>
-						<Experience />
-					</Fade>
-				</Container>
-			</div>
+			<Container className='container-box rounded'>
+				<Slide bottom duration={500}>
+					<hr />
+					<Skills />
+				</Slide>
+			</Container>
+
+			<Container className='container-box rounded'>
+				<Fade duration={500}>
+					<hr />
+
+					<Experience />
+				</Fade>
+			</Container>
+
+			<Container className='container-box rounded'>
+				<Slide bottom duration={500}>
+					<hr />
+					<TimeLine />
+				</Slide>
+			</Container>
+
+			<Container className='container-box rounded'>
+				<Fade duration={500}>
+					<hr />
+					<ContactForm />
+				</Fade>
+			</Container>
+
+			<hr />
+			<FooterPanel />
 		</div>
 	);
 };
