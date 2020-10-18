322d79b858f0e497c160015dba846ef27426934d
Prashant Raut <prashantraut33@gmail.com>
2020-10-18 09:16:44 +0530

    'initial Commit completed MyNavbar component'

---
+++ src/components/Navbar/mynavbar.components.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../../assets/icons/logo2.JPG';
+import './mynavbar.components.css';
+
+const MyNavbar = () => {
+  return (
+    <>
+      <Navbar
+        fixed="top"
+        collapseOnSelect
+        expand="md"
+        variant="dark"
+        className="animate-navbar nav-theme justify-content-between"
+      >
+        <Navbar.Brand href="#home">
+          <img className="logo" src={Logo} alt="my profile logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Skills">Skills</Nav.Link>
+            <Nav.Link href="#Experience">Experience</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default MyNavbar;
