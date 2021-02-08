b709e4387eb5d618d67349b55f8945294f0d6907
Prashant Raut <spashraut@gmail.com>
2021-02-08 21:53:12 +0530

    Resume link added

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -55,7 +55,11 @@ const About = () => {
 										</a>
 									</div>
 									<div>
-										<a href='#' target='_blank' rel='noopener noreferrer'>
+										<a
+											href='https://drive.google.com/drive/u/0/my-drive'
+											target='_blank'
+											rel='noopener noreferrer'
+										>
 											<Button className='m-2' variant='outline-success'>
 												My Resume
 											</Button>
